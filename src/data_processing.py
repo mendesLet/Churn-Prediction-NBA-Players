e6aeb75905ced1ede68a6e4fdcd168b7742ac526
mendesLet <limendes1305@gmail.com>
2023-08-05 14:50:54 -0300

    Trying new data

---
+++ src/data_processing.py
@@ -0,0 +1,24 @@
+### This is a script for cleaning data and feature adding 
+### The features for the dataset will be
+### From season_stats: G, GS, MP, PER, TS%, OWS, DWS, WS, BPM, FG%, 2P%, 3P%
+### From player_stats: name, year_start, year_end,postion, height, weight, birth_date
+### To add: age_start, age_end, time_active, is_churn
+
+# Remover unwanted features
+
+# Unir ambos csv
+
+# Remover jogadores de 1950 a 1979
+
+# Calcular idade (age_start, age_end) adicionar colunas
+# Calcular tempo ativo (time_active) adicionar coluna
+
+# Calcular média de year_end para intervalos de anos 
+# Medir se é churn
+# Criar coluna de churn
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
